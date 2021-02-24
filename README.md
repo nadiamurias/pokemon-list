8c8b4603cdefba7324a9b07ffbb40b9eaf9c79bb
Nadia Murias <nadiamurias@gmail.com>
2021-02-24 08:51:01 +0100

    Create initial structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# Módulo 3 - React
+
+## Evaluación intermedia Nadia Murias
+
+Aquí podréis ver el ejercicio solicitado ha realizar para la evaluación intermedia del tercer módulo sobre React.
---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Data from "../data/data.json";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        {" "}
+        {/* a esto se le llama fragment */}
+        <li>Uno</li>
+        <li>Dos</li>
+      </>
+    );
+  }
+}
+export default App;
